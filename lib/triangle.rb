5f15be8355f8f7fa513f34eba7caadd8d9db90c4
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-05 23:53:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,12 @@
 class Triangle
+  attr_accessor :side_1, :side_2, :side_3
    
    
-   def initialize
+   def initialize(side_1, side_2, side_3)
+     @side_1 = side_1
+     @side_2 = side_2
+     @side_3 = side_3
+   end 
+   
+   def kind 
 end
