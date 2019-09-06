8a5c421aefec83e32f5ea06dff827bcdf1b695ef
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-06 00:22:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,8 +22,8 @@ class Triangle
      def valid?
     valid_traingle = [(side_1 + side_2 > side_3), (side_2 + side_3 > side_1), (side_1 + side_3 > side_2)]
     [side_1, side_2, side_3].each do |side|
-      valid_traingle << "not valid" if side <=0
-      raise TriangleError if valid_traingle.include?("not valid")
+      valid_traingle << false if side <=0
+      raise TriangleError if valid_traingle.include?(false)
     end 
   end 
 
