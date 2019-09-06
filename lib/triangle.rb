cee6c4f8290c8742ff0fbf8bb88f2e993efdb342
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-06 00:19:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
      valid? 
      if side_1 == side_2 && side_2 == side_3
          return :equilateral
-     elsif side_1 == side_2 || side_2 == side_3 || side_2 == side_3
+     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
       return :isosceles
     else 
       return :scalene
